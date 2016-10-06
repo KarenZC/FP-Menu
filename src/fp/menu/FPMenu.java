4d446b68d7d9ef5328b3235d25739f8e3d9aade5
karen <karen@KarenZ>
2016-10-06 11:47:18 -0500

    Menu Java

--- src/fp/menu/FPMenu.java
+++ src/fp/menu/FPMenu.java
@@ -4,13 +4,16 @@
  * and open the template in the editor.
  */
 package fp.menu;
-
+import java.util.*;
 /**
  *
  * @author alumno
  */
 public class FPMenu {
-    
+    int m;
+    float a;
+    a=CalculaAreas(m);
+    muestraResultado(a);
 
     /**
      * @param args the command line arguments
@@ -19,4 +22,21 @@ public class FPMenu {
         // TODO code application logic here
     }
     
+    public static void muestraResultado (float area, int r){
+        System.out.println("el area del");
+    }
+    public static int menu (){
+    Scanner entrada= new Scanner(System.in);
+    int option;
+    do{ 
+        System.out.println("Calculo de áreas");
+        System.out.println("Menu");
+        System.out.println("Introduce la opción deseada");
+        System.out.println("1.Área de cuadrados");
+        System.out.println("2.Área de círculos");
+        System.out.println("3.Área de triangulos");
+        System.out.println("0.Salir");
+        
+      }
+   }
 }
